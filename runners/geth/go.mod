1640f4bb70c22d4bec0b82ff940ecff1c3048c5e
Ziyad Edher <ziyad.edher@gmail.com>
2022-10-16 00:27:02 -0700

    Implement `geth` runner

---
+++ runners/geth/go.mod
@@ -0,0 +1,36 @@
+module evm-bench/runners/runner-geth
+
+go 1.19
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/VictoriaMetrics/fastcache v1.12.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ethereum/go-ethereum v1.10.25 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
+	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
+	github.com/holiman/uint256 v1.2.1 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/tsdb v0.10.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/spf13/cobra v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.5.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
+	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
---
+++ runners/geth/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/spf13/cobra"
+)
+
+var (
+	contractCodePath string
+	calldata         string
+	numRuns          int
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+		os.Exit(1)
+	}
+}
+
+var cmd = &cobra.Command{
+	Use:   "runner-geth",
+	Short: "go-ethereum runner for evm-bench",
+	Run: func(_ *cobra.Command, _ []string) {
+		contractCodeHex, err := os.ReadFile(contractCodePath)
+		check(err)
+
+		contractCodeBytes := common.Hex2Bytes(string(contractCodeHex))
+		calldataBytes := common.Hex2Bytes(calldata)
+
+		zeroAddress := common.BytesToAddress(common.FromHex("0x0000000000000000000000000000000000000000"))
+		callerAddress := common.BytesToAddress(common.FromHex("0x1000000000000000000000000000000000000001"))
+
+		config := params.MainnetChainConfig
+		rules := config.Rules(config.BerlinBlock, false)
+		defaultGenesis := core.DefaultGenesisBlock()
+		genesis := &core.Genesis{
+			Config:     config,
+			Coinbase:   defaultGenesis.Coinbase,
+			Difficulty: defaultGenesis.Difficulty,
+			GasLimit:   defaultGenesis.GasLimit,
+			Number:     config.BerlinBlock.Uint64(),
+			Timestamp:  defaultGenesis.Timestamp,
+			Alloc:      defaultGenesis.Alloc,
+		}
+
+		statedb, err := state.New(common.Hash{}, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+		check(err)
+
+		zeroValue := big.NewInt(0)
+		gasLimit := uint64(1000000000)
+
+		createMsg := types.NewMessage(callerAddress, &zeroAddress, 0, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, contractCodeBytes, types.AccessList{}, false)
+		statedb.PrepareAccessList(callerAddress, &zeroAddress, vm.ActivePrecompiles(rules), createMsg.AccessList())
+
+		blockContext := core.NewEVMBlockContext(genesis.ToBlock().Header(), nil, &zeroAddress)
+		txContext := core.NewEVMTxContext(createMsg)
+		evm := vm.NewEVM(blockContext, txContext, statedb, config, vm.Config{})
+		_, contractAddress, _, err := evm.Create(vm.AccountRef(callerAddress), contractCodeBytes, 1000000000, new(big.Int))
+		check(err)
+
+		msg := types.NewMessage(callerAddress, &contractAddress, 1, zeroValue, gasLimit, zeroValue, zeroValue, zeroValue, calldataBytes, types.AccessList{}, false)
+		for i := 0; i < numRuns; i++ {
+			snapshot := statedb.Snapshot()
+			statedb.PrepareAccessList(msg.From(), msg.To(), vm.ActivePrecompiles(rules), msg.AccessList())
+
+			start := time.Now()
+			_, _, err := evm.Call(vm.AccountRef(callerAddress), *msg.To(), msg.Data(), msg.Gas(), msg.Value())
+			timeTaken := time.Since(start)
+
+			fmt.Println(timeTaken.Milliseconds())
+
+			check(err)
+
+			statedb.RevertToSnapshot(snapshot)
+		}
+	},
+}
+
+func init() {
+	cmd.Flags().StringVar(&contractCodePath, "contract-code-path", "", "Path to the hex contract code to deploy and run")
+	cmd.MarkFlagRequired("contract-code-path")
+	cmd.Flags().StringVar(&calldata, "calldata", "", "Hex of calldata to use when calling the contract")
+	cmd.MarkFlagRequired("calldata")
+	cmd.Flags().IntVar(&numRuns, "num-runs", 0, "Number of times to run the benchmark")
+	cmd.MarkFlagRequired("num-runs")
+}
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
