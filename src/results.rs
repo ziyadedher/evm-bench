a7829dea24a6ce8f802c8183bae2ee92804a0fe3
Ziyad Edher <ziyad.edher@gmail.com>
2022-10-25 23:42:05 -0700

    Improve results formatting

--- src/results.rs
+++ src/results.rs
@@ -102,8 +102,62 @@ pub fn print_results(results_file_path: &Path) -> Result<(), Box<dyn error::Erro
     runs.sort_by_key(|(b, _)| b.clone());
 
     let mut runner_times = HashMap::<String, Vec<Duration>>::new();
+    for (_, benchmark_runs) in runs.iter() {
+        runner_names.iter().for_each(|runner_name| {
+            let run = benchmark_runs.get(runner_name).unwrap();
+            let avg_run_time = run
+                .run_times
+                .iter()
+                .fold(Duration::ZERO, |a, v| a + v.clone())
+                .div_f64(run.run_times.len() as f64);
+            runner_times
+                .entry(runner_name.clone())
+                .or_default()
+                .push(avg_run_time);
+        });
+    }
+
+    runner_names.sort_by_key(|runner_name| {
+        runner_times
+            .get(runner_name)
+            .unwrap()
+            .into_iter()
+            .fold(Duration::ZERO, |a, v| a + v.clone())
+    });
 
     let mut builder = Builder::default();
+    let average_runner_times = runner_times
+        .iter()
+        .map(|(name, times)| (name.clone(), times.iter().sum::<Duration>()))
+        .collect::<HashMap<String, Duration>>();
+    let mut record = vec!["sum".to_string()];
+    record.extend(
+        runner_names
+            .iter()
+            .map(|runner_name| average_runner_times.get(runner_name))
+            .map(|val| Some(format!("{:?}", val?)))
+            .map(|s| s.unwrap_or_default()),
+    );
+    builder.add_record(record);
+    let min_runner_time = average_runner_times
+        .values()
+        .min()
+        .ok_or("could not get minimum runner time")?;
+    let mut record = vec!["relative".to_string()];
+    record.extend(
+        runner_names
+            .iter()
+            .map(|runner_name| {
+                Some(
+                    average_runner_times.get(runner_name)?.as_secs_f64()
+                        / min_runner_time.as_secs_f64(),
+                )
+            })
+            .map(|val| Some(format!("{:.3?}", val?)))
+            .map(|s| s.unwrap_or_default()),
+    );
+    builder.add_record(record);
+
     for (benchmark_name, benchmark_runs) in runs.iter() {
         let vals = runner_names.iter().map(|runner_name| {
             let run = benchmark_runs.get(runner_name)?;
@@ -126,20 +180,6 @@ pub fn print_results(results_file_path: &Path) -> Result<(), Box<dyn error::Erro
         );
         builder.add_record(record);
     }
-
-    let average_runner_times = runner_times
-        .into_iter()
-        .map(|(name, times)| (name, times.iter().sum::<Duration>()))
-        .collect::<HashMap<String, Duration>>();
-    let mut record = vec!["sum".to_string()];
-    record.extend(
-        runner_names
-            .iter()
-            .map(|runner_name| average_runner_times.get(runner_name))
-            .map(|val| Some(format!("{:?}", val?)))
-            .map(|s| s.unwrap_or_default()),
-    );
-    builder.add_record(record);
 
     let mut columns = vec!["".to_owned()];
     columns.extend(runner_names);
