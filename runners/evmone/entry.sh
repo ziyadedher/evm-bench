2059f24d2094f3fdc5f6ecda3fe7be45d10a2624
Paweł Bylica <pawel@hepcolgum.band>
2022-12-20 12:53:59 +0100

    Build evmone runner in Release

--- runners/evmone/entry.sh
+++ runners/evmone/entry.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 {
-  cmake -S . -B build && cmake --build build --parallel
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --parallel
 } > /dev/null
 build/runner $@
